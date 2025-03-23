15b9736af68fbb658cfc1cb8f848221ffeaa889c
Mehmet kasım YURTSEVER <mehmetkasin@gmail.com>
2025-03-23 03:48:32 +0300

    UnityDersleri

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,50 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class PlayerController : MonoBehaviour
 {
-   
-    public InputAction MoveAction;
-    
-    void Start()
-    {
-        //Debug.Log( "Başlangıç fonksiyonu çalıştı.");
-        //QualitySettings.vSyncCount = 0;
-        //Application.targetFrameRate = 150;
-        MoveAction.Enable();
-    }
+  // Variables related to player character movement
+  public InputAction MoveAction;
+  Rigidbody2D rigidbody2d;
+  Vector2 move;
+  public float speed = 3.0f;
 
-  
-    void Update()
-    {
-    /*    float horizontal = 0.0f;
-       if (LeftAction.IsPressed())
-    {
-        horizontal = -1.0f;
-    }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
 
-        horizontal = 1.0f;
 
-        }
-        
-        float vertical = 0.0f;
-        if (Keyboard.current.upArrowKey.isPressed)
-        {
-        vertical = 1.0f;
-        }
-        else if (Keyboard.current.downArrowKey.isPressed)
-       {
-        vertical = -1.0f;
-        }
-*/
+  // Variables related to the health system
+  public int maxHealth = 5;
+  public int health { get { return currentHealth; } }
+  int currentHealth = 1;
+
+  public float timeInvincible = 2.0f;
+  bool isInvincible;
+  float damageCooldown;
+  Animator animator;
+  Vector2 moveDirection = new Vector2(1, 0);
+
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    MoveAction.Enable();
+    rigidbody2d = GetComponent<Rigidbody2D>();
+    animator = GetComponent<Animator>();
+    currentHealth = maxHealth;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    move = MoveAction.ReadValue<Vector2>();
+    if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
     {
-     Vector2 move = MoveAction.ReadValue<Vector2>();
-     Debug.Log(move);
-     Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
-     transform.position = position;
-     }
-        //Debug.Log( "Güncelleme fonksiyonu çalışıyor.");
+      moveDirection.Set(move.x, move.y);
+      moveDirection.Normalize();
+
     }
+    animator.SetFloat("Look X", moveDirection.x);
+    animator.SetFloat("Look Y", moveDirection.y);
+    animator.SetFloat("Speed", move.magnitude);
+
+
+    if (isInvincible)
+    {
+      damageCooldown -= Time.deltaTime;
+      if (damageCooldown < 0)
+      {
+        isInvincible = false;
+      }
+    }
+  }
+
+
+  // FixedUpdate has the same call rate as the physics system
+  void FixedUpdate()
+  {
+    Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
+    rigidbody2d.MovePosition(position);
+  }
+
+
+  public void ChangeHealth(int amount)
+  {
+    if (amount < 0)
+    {
+      if (isInvincible)
+      {
+        return;
+      }
+      isInvincible = true;
+      damageCooldown = timeInvincible;
+      animator.SetTrigger("Hit");
+    }
+    currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+    //Debug.Log(currentHealth + "/" + maxHealth);
+    UIHandler.instance.SetHealthValue(currentHealth / (float)maxHealth);
+  }
+
 }
